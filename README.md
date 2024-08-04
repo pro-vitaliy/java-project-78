f3f1ed61f25a0b178a908e8f4ef2d295430dc6c6
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-08-04 13:12:05 +0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/pro-vitaliy/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/pro-vitaliy/java-project-78/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/3537eaa03376315b7ecb/maintainability)](https://codeclimate.com/github/pro-vitaliy/java-project-78/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/3537eaa03376315b7ecb/test_coverage)](https://codeclimate.com/github/pro-vitaliy/java-project-78/test_coverage)
---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    checkstyle
+    jacoco
+    id("io.freefair.lombok") version "8.6"
+    id ("com.github.ben-manes.versions") version "0.51.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStackTraces = true
+        showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
