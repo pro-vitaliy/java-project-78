28caa143097dea5ade1ef1fbcca75c529e4be1b6
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-08-07 22:14:27 +0500

    refactor MapSchema add shape schema

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -4,17 +4,28 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public class MapSchema extends BaseSchema<Map<?, ?>> {
+public class MapSchema<K, V> extends BaseSchema<Map<K, V>> {
 
-    public MapSchema required() {
-        Predicate<Map<?, ?>> notNull = Objects::nonNull;
+    public MapSchema<K, V> required() {
+        Predicate<Map<K, V>> notNull = Objects::nonNull;
         addValidationRule("required", notNull);
         return this;
     }
 
-    public MapSchema sizeof(int size) {
-        Predicate<Map<?, ?>> isSizeOf = m -> !Objects.nonNull(m) || m.size() == size;
+    public MapSchema<K, V> sizeof(int size) {
+        Predicate<Map<K, V>> isSizeOf = m -> !Objects.nonNull(m) || m.size() == size;
         addValidationRule("sizeof", isSizeOf);
+        return this;
+    }
+
+    public MapSchema<K, V> shape(Map<K, BaseSchema<V>> schema) {
+
+        Predicate<Map<K, V>> isShape = m -> {
+            return m.keySet().stream()
+                    .allMatch(k -> schema.get(k).isValid(m.get(k)));
+        };
+
+        addValidationRule("shape", isShape);
         return this;
     }
 }
