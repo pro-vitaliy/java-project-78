74e610df15c2b48f66ae6bf2c032c39f3ca39df1
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-08-11 02:18:20 +0500

    fix MapSchema

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -6,12 +6,6 @@ import java.util.function.Predicate;
 
 public final class MapSchema extends BaseSchema<Map<?, ?>> {
 
-    public MapSchema required() {
-        Predicate<Map<?, ?>> notNull = Objects::nonNull;
-        addValidationRule("required", notNull);
-        return this;
-    }
-
     public MapSchema sizeof(int size) {
         Predicate<Map<?, ?>> isSizeOf = m -> !Objects.nonNull(m) || m.size() == size;
         addValidationRule("sizeof", isSizeOf);
