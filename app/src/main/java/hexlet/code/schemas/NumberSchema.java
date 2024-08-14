b50f5c86f91b10ab0d6e6b4f393fc93da22ae69b
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-08-14 21:54:15 +0500

    refactor NumberSchema: add constructor with default predicate

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -5,14 +5,24 @@ import java.util.function.Predicate;
 
 public final class NumberSchema extends BaseSchema<Integer> {
 
+    public NumberSchema() {
+        Predicate<Integer> numNotNull = Objects::nonNull;
+        addValidationRule("required", numNotNull);
+    }
+
+    public NumberSchema required() {
+        markAsRequired();
+        return this;
+    }
+
     public NumberSchema positive() {
-        Predicate<Integer> numIsPositive = n -> !Objects.nonNull(n) || n > 0;
+        Predicate<Integer> numIsPositive = n -> n > 0;
         addValidationRule("positive", numIsPositive);
         return this;
     }
 
     public NumberSchema range(int begin, int end) {
-        Predicate<Integer> numInRange = n -> !Objects.nonNull(n) || (n >= begin && n <= end);
+        Predicate<Integer> numInRange = n -> n >= begin && n <= end;
         addValidationRule("range", numInRange);
         return this;
     }
