828e0deb78462550b68768b93da54f1e1c827b13
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-08-14 21:54:23 +0500

    refactor StringSchema: add constructor with default predicate

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -5,22 +5,25 @@ import java.util.function.Predicate;
 
 public final class StringSchema extends BaseSchema<String> {
 
-    @Override
-    public StringSchema required() {
+    public StringSchema() {
         Predicate<String> stringNonNull = Objects::nonNull;
         Predicate<String> stringNotEmpty = s -> !s.isEmpty();
         addValidationRule("required", stringNonNull.and(stringNotEmpty));
+    }
+
+    public StringSchema required() {
+        markAsRequired();
         return this;
     }
 
     public StringSchema contains(String str) {
-        Predicate<String> isContains = s -> !Objects.nonNull(s) || s.contains(str);
+        Predicate<String> isContains = s -> s.contains(str);
         addValidationRule("contains", isContains);
         return this;
     }
 
     public StringSchema minLength(int len) {
-        Predicate<String> minLen = s -> !Objects.nonNull(s) || s.length() >= len;
+        Predicate<String> minLen = s -> s.length() >= len;
         addValidationRule("minLength", minLen);
         return this;
     }
