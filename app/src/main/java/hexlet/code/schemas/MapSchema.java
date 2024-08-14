852ca5f0631ae5c6d1b4701f11357ef3b07da6b9
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-08-14 21:54:03 +0500

    refactor MapSchema: add constructor with default predicate

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -6,14 +6,24 @@ import java.util.function.Predicate;
 
 public final class MapSchema extends BaseSchema<Map<?, ?>> {
 
+    public MapSchema() {
+        Predicate<Map<?, ?>> mapNotNull = Objects::nonNull;
+        addValidationRule("required", mapNotNull);
+    }
+
+    public MapSchema required() {
+        markAsRequired();
+        return this;
+    }
+
     public MapSchema sizeof(int size) {
-        Predicate<Map<?, ?>> isSizeOf = m -> !Objects.nonNull(m) || m.size() == size;
+        Predicate<Map<?, ?>> isSizeOf = m -> m.size() == size;
         addValidationRule("sizeof", isSizeOf);
         return this;
     }
 
     public <T> MapSchema shape(Map<String, BaseSchema<T>> schemas) {
-        Predicate<Map<?, ?>> isShape = m -> m == null || m.keySet().stream()
+        Predicate<Map<?, ?>> isShape = m -> m.keySet().stream()
                 .allMatch(k -> schemas.get(k).isValid((T) m.get(k)));
 
         addValidationRule("shape", isShape);
